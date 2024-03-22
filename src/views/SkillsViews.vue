1c5472df5e260c5064edb543f84fa383cba80a32
Quentin <118888713+QuentinB0@users.noreply.github.com>
2024-03-22 16:56:23 +0100

    feat(hardSkills): add object skills for skill content

--- src/views/SkillsViews.vue
+++ src/views/SkillsViews.vue
@@ -1,13 +1,25 @@
 <script setup>
+import { ref } from 'vue'
+import { skills } from '../assets/js/hardSkills.js'
+
+const hardSkills = ref(skills)
 
 </script>
 
 <template>
   <main>
-   skills
+    <div v-for="skill in skills">
+    <div class="card">
+      <h2 class="card-title" v-html="skill.logo"></h2>
+      <p class="card-content">{{ skill.language }}</p>
+    </div>
+    </div>
   </main>
 </template>
 
+<style scoped lang="scss">
+
+</style>
 <style scoped lang="scss">
 
 </style>
